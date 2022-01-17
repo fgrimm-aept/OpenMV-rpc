d997426e5f86be6c35ea1ac0e93f6d980adcba65
Florens Grimm <fgrimm@ep1.rub.de>
2022-01-17 16:18:58 +0100

    Funktion geändert

--- output_data.py
+++ output_data.py
@@ -4,15 +4,16 @@ import numpy as np
 
 
 # Gibt das numpy array als return value zurück
-def return_array(np_arr):
-    return np_arr
+def return_array():
+    arr = np.load('Test.npy')
+    return arr
 
 
 # Darstellung für print statements von numpy arrays (ändert nichts am Array selber)
 np.set_printoptions(threshold=sys.maxsize, edgeitems=sys.maxsize)
 
 # Lade das von der Kamera gespeicherte Numpy array
-arr = np.load('Test.npy')
+
 
 print(arr)
 # func call
